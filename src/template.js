ad94a53260d72a833f7511139cd896de535ee864
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-02-17 02:49:42 +0100

    Add github repo link

--- src/template.js
+++ src/template.js
@@ -2,7 +2,7 @@ import sheet from "./style.css";
 import displayHome from "./home.js"
 import displayMenu from "./menu.js"
 import displayContact from "./contact.js"
-import {createNavItem, getNavItem, setSelectedNavItem} from "./navSelection.js";
+import { createNavItem, getNavItem, setSelectedNavItem } from "./navSelection.js";
 
 function createHeaderMarkup() {
     const title = document.createElement("span");
@@ -52,14 +52,21 @@ function createFooterMarkup() {
     const footerLegals = document.createElement("div");
     footerLegals.classList = ["footer-legals"];
 
-     const freepik1 = createImageSourceLink("Image by freepik", "https://www.freepik.com/free-photo/top-view-delicious-pizza-wooden-table_9772390.htm\
+    const githubSvg = document.createElement("span");
+    githubSvg.classList.add("github-svg");
+    const githubAnchor = document.createElement("a");
+    githubAnchor.href = "https://github.com/arnaudSanchez1816/odin-restaurant";
+    githubAnchor.appendChild(githubSvg);
+    footerLegals.appendChild(githubAnchor);
+
+    const freepik1 = createImageSourceLink("Image by freepik", "https://www.freepik.com/free-photo/top-view-delicious-pizza-wooden-table_9772390.htm\
      #fromView=search&page=1&position=7&uuid=87afdb5d-7e7f-47f2-9ca0-94ccf4abb371&query=Pizza");
-     footerLegals.appendChild(freepik1);
+    footerLegals.appendChild(freepik1);
 
 
-     const freepik2 = createImageSourceLink("Image by freepik", "https://www.freepik.com/free-psd/top-view-delicious-pizza_370860293.htm\
+    const freepik2 = createImageSourceLink("Image by freepik", "https://www.freepik.com/free-psd/top-view-delicious-pizza_370860293.htm\
      #fromView=search&page=1&position=27&uuid=b001b61e-bb13-4a58-9fc5-3d2a9d8d79e6&query=pizza");
-     footerLegals.appendChild(freepik2);
+    footerLegals.appendChild(freepik2);
 
     const footer = document.createElement("footer");
     footer.appendChild(footerContent);
@@ -99,7 +106,7 @@ function buildWebsite() {
 
     displayHome();
     const homeNavItem = getNavItem("nav-home");
-    if(homeNavItem) {
+    if (homeNavItem) {
         setSelectedNavItem(homeNavItem);
     }
 }
